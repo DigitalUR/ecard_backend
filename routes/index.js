4109e57cc8646f5f7842ed97fd3dce6641d960a5
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-09-09 21:44:19 +0200

    feat: Redirect uri active

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ import { testEsignet } from "../controllers/oauth2_controllers.js";
 
 const routes = Router();
 routes.route("/").get(testEsignet);
+routes.route('/apw/oauth2/esignet/callback').get((req, res) => {
+    console.log(req.query);
+})
 
 export default  routes;
