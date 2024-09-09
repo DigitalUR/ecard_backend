2c620da374f636d9864840c323078fce4e8559e1
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-09-09 21:47:06 +0200

    fix: Simple typo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { testEsignet } from "../controllers/oauth2_controllers.js";
 
 const routes = Router();
 routes.route("/").get(testEsignet);
-routes.route('/apw/oauth2/esignet/callback').get((req, res) => {
+routes.route('/api/oauth2/esignet/callback').get((req, res) => {
     console.log(req.query);
 })
 
