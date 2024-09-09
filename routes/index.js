7a142b86a51d035b4bcb860e73b6025d900f7190
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-09-09 21:54:19 +0200

    fix: return statement on redirect url

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const routes = Router();
 routes.route("/").get(testEsignet);
 routes.route('/api/oauth2/esignet/callback').get((req, res) => {
     console.log(req.query);
+    return res.send('ok');
 })
 
 export default  routes;
