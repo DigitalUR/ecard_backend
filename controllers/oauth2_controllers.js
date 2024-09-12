caef684a41928f89df5094beee64af4782c7984f
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-09-12 04:53:47 +0200

    feat: picture loading endpoint

--- controllers/oauth2_controllers.js
+++ controllers/oauth2_controllers.js
@@ -3,10 +3,9 @@ import { esignet } from '../services/oauth2Service.js';
 import { getInfo } from '../services/dataService.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
-
-const photos = [];
 
 const oauth2Esignet = async (req, res) => {
     const { code } = req.query;
@@ -15,12 +14,16 @@ const oauth2Esignet = async (req, res) => {
 
     try {
         const userEsgnetInfos = await esignet(code);
-        const {picture, ...rest} = userEsgnetInfos;
-
         const academic = await getInfo(userEsgnetInfos.email);
-        const combinedInfo = { ...academic, ...rest};
-
-        photos.push({id:academic.regno, pic:picture});
+        // bobox idea
+        const image = userEsgnetInfos.picture;
+        
+        const uuidName = 'a100';
+        fs.writeFileSync(`${uuidName}.txt`, image);
+        userEsgnetInfos.picture = uuidName;
+        
+        const combinedInfo = { ...academic, ...userEsgnetInfos};
+        //bobox idea
 
         const token = jwt.sign(combinedInfo, process.env.JWT_SECRET_KEY);
 
@@ -31,17 +34,50 @@ const oauth2Esignet = async (req, res) => {
     }
 };
 
-const getPhoto = (req, res) => {
-    const { regno } = req.params;
-    const pic = photos.find(photo=> photo.id === regno);
+const dataDemo = (req, res) => {
+    res.status(httpStatus.OK).json({
+        regNo:222004312,
+        college:'College of science and technology',
+        school:'School og ICT',
+        department:'Computer science',
+        active:true,
+        enrolledAt:2021,
+        completionTime:2025
+    });
+}
+const getImage = (req, res) => {
+    const imageFileName = req.imageFileName;
+    const filePath = `./${imageFileName}.txt`;
 
-    const index = photos.findIndex(photo => photo.id === regno);
-    photos.splice(index, 1);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading the file: ${err.message}`);
+            return;
+        }
 
-    res.status(httpStatus.OK).json({photo:pic.pic});
-};
+    });
+
+fs.unlink(filePath, (err) => {
+    if (err) {
+        console.error(`Error deleting the file: ${err.message}`);
+        return;
+    }
+
+});
+
+    res.status(httpStatus.OK).json({
+        regNo:222004312,
+        college:'College of science and technology',
+        school:'School og ICT',
+        department:'Computer science',
+        active:true,
+        enrolledAt:2021,
+        completionTime:2025
+    });
+}
 
 export {
     oauth2Esignet,
-    getPhoto
+    dataDemo,
+    getImage,
 };
--- routes/ouathRoutes.js
+++ routes/ouathRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
-import { getPhoto, oauth2Esignet} from "../controllers/oauth2_controllers.js";
+import { oauth2Esignet, dataDemo,getImage } from "../controllers/oauth2_controllers.js";
 
 const ouath2Routes = Router();
-ouath2Routes.route('/esignet/callback').get(oauth2Esignet);
-ouath2Routes.route('/picture/:regno').get(getPhoto);
+ouath2Routes.route("/").get(dataDemo);
+ouath2Routes.route('/esignet/callback').get(oauth2Esignet)
+ouath2Routes.route('/getImage').get(getImage)
+
 export default ouath2Routes;
