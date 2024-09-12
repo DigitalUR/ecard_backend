1131c6fa8ce7ad4b01c01b903d481a88fba7ccb9
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-09-12 06:17:45 +0200

    feat: picture loading endpoint

--- routes/ouathRoutes.js
+++ routes/ouathRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 
-import { oauth2Esignet, dataDemo,getImage } from "../controllers/oauth2_controllers.js";
+import { oauth2Esignet, getImage } from "../controllers/oauth2_controllers.js";
 
 const ouath2Routes = Router();
-ouath2Routes.route("/").get(dataDemo);
 ouath2Routes.route('/esignet/callback').get(oauth2Esignet)
 ouath2Routes.route('/getImage/:imageFileName').get(getImage)
 
